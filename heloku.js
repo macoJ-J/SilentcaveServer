98a4c749b153b99599f902d4a87902e4a5f85eb0
nagaoyosuke <hw17a124@oecu.jp>
2017-12-25 16:21:59 +0900

    no message

--- heloku.js
+++ heloku.js
@@ -37,14 +37,16 @@ wss.on('connection', function(ws) {
 				player1 = ws;
 				player1.send(JSON.stringify({"ID":1,"name":"my"}));
 				player1a = 1;
+				wss.broadcast ("player1");
 			break;
 			case 1:
 				player2 = ws;
 				player2.send(JSON.stringify({"ID":2,"name":"my"}));
 				player2a = 1;
+				wss.broadcast ("player2");
 			break;
 			};
-			
+
 			count ++;
 		//	if (count >= 2){
 		//		count = 0;
@@ -52,15 +54,15 @@ wss.on('connection', function(ws) {
 
 			//誰からでもメッセージを受信した時
 			ws.on ('message', function (message) {
-				//var now = new Date();
-				//console.log (now.toLocaleString() + ' Received: %s', message);
-				//wss.broadcast (message);
-				
+				var now = new Date();
+				console.log (now.toLocaleString() + ' Received: %s', message);
+				wss.broadcast (message);
+
 			});
 			//player1からメッセージを受信した時
 			if (player1a === 1) {
 				player1.on('message', function (pl2) {
-					
+
 					if (~pl2.indexOf('HP')) {
 						//data2にhpを含む場合の処理
 						console.log (now.toLocaleString() + ' player01: %s', pl2.substring( pl2.indexOf('HP'),pl2.indexOf('HP') +8 ));
@@ -69,20 +71,20 @@ wss.on('connection', function(ws) {
 
 					}
 					if (player2a === 1) {
-						
-										
+
+
 						if (~pl2.indexOf('my')) {
 							//data2にmyを含む場合の処理
 							pl2 = pl2.replace( /my/g , "opponent" ) ;
 						}else if ( ~pl2.indexOf('opponent')) {
 							pl2 = pl2.replace( /opponent/g , "my" ) ;
 						};
-						
+
 												var data2 = JSON.parse(pl2);
 						player2.send(JSON.stringify(data2));
 					};
 				});
-				
+
 				//切断時
 				player1.on('close', function () {
 						player1a = 0;
@@ -90,11 +92,11 @@ wss.on('connection', function(ws) {
 						console.log ('player1:reset');
 				});
 			};
-			
+
 			//player2からメッセージを受信した時
-			if (player2a === 1){	
+			if (player2a === 1){
 				player2.on('message', function (pl1) {
-					
+
 					if (~pl1.indexOf('HP')) {
 						//data2にhpを含む場合の処理
 						console.log (now.toLocaleString() + ' player02: %s', pl1.substring( pl1.indexOf('HP'),pl1.indexOf('HP') + 8 ));
@@ -102,26 +104,26 @@ wss.on('connection', function(ws) {
 						wss.broadcast (pl1);
 
 					}
-										
+
 					if (player1a === 1) {
-							
+
 						if (~pl1.indexOf('my')) {
 							//data2にmyを含む場合の処理
 							pl1 = pl1.replace( /my/g , "opponent" ) ;
 						}else if ( ~pl1.indexOf('opponent')) {
 							pl1 = pl1.replace( /opponent/g , "my" ) ;
-						};	
-						
+						};
+
 						/*if ( data1.match(/my/)) {
 							//data2にmyを含む場合の処理
 							data1 = data1.replace( /my/g , "opponent" ) ;
 						}else if ( ~data1.indexOf('opponent')) {
 							data1 = data1.replace( /opponent/g , "my" ) ;
 						};
-						*/			
+						*/
 						var data1 = JSON.parse(pl1);
 						player1.send(JSON.stringify(data1));
-						
+
 					};
 				});
 				//切断時
@@ -134,7 +136,7 @@ wss.on('connection', function(ws) {
 						}
 						console.log ('player2:reset');
 				});
-				
+
 			};
 
 });
