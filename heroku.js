f732f1e9a3420937bb7baf50e839643d455dcbea
o <hw17a114@oecu.jp>
2021-08-20 14:06:21 +0900

    no message

--- heroku.js
+++ heroku.js
@@ -5,8 +5,6 @@ var WebSocketServer = require('ws').Server
 	, port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
-app.use("/express", express.static(__dirname + "/node_modules/express/lib/"));
-
 
 var server = http.createServer(app);
 server.listen(port,process.env.IP);
