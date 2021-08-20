d011914a9aebcaaa17e0c0e930a1ae07b7d26b7a
o <hw17a114@oecu.jp>
2021-08-20 10:55:09 +0900

    no message

--- public/script/display-active-player.js
+++ public/script/display-active-player.js
@@ -1,11 +1,8 @@
 function displayActivePlayer()
 {
-    const fs = require('fs');
     const activeuserjson = require(__dirname +'/../json/activeplayerdata.json');
     
     console.log(activeuserjson);
     console.log(activeuserjson.length);
     document.getElementById("getData").innerHTML = "現在接続中のプレイヤーは" + activeuserjson.length + "名です。";
 }
-
-window.onload = displayActivePlayer;
